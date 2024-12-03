1aae003ea80f653b0f786290a38426e85f0b7896
BLACKEYE-ASTRO <akashshingare2003@gmail.com>
2024-12-03 19:52:50 +0530

    All Upadated

--- src/components/TaskFilters.jsx
+++ src/components/TaskFilters.jsx
@@ -4,15 +4,15 @@ const TaskFilters = ({ activeFilter, onFilterChange }) => {
   const filters = ["All", "Completed", "Pending", "Overdue"];
 
   return (
-    <div className="flex gap-4 mb-6 ml-4 mt-5">
+    <div className="flex flex-wrap gap-4 mb-6 ml-4 mt-5">
       {filters.map((filter) => (
         <button
           key={filter}
           onClick={() => onFilterChange(filter.toLowerCase())}
-          className={`px-4 py-2 rounded-lg ${
+          className={`min-w-[120px] px-4 py-2 rounded-lg ${
             activeFilter === filter.toLowerCase()
-              ? "bg-purple-600 text-sm md:text-xl text-white"
-              : "bg-gray-700 text-sm md:text-xl text-gray-300 hover:bg-gray-600"
+              ? "bg-purple-600 text-sm md:text-base text-white"
+              : "bg-gray-700 text-sm md:text-base text-gray-300 hover:bg-gray-600"
           }`}
         >
           {filter}
